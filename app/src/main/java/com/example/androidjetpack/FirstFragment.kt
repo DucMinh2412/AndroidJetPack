fa95d282705a5fe55a2918bca1032a5b735d5a81
Duc Minh <https://github.com/DucMinh2412/D-n-1---PT14353>
2020-12-21 17:24:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/androidjetpack/FirstFragment.kt
@@ -0,0 +1,36 @@
+package com.example.androidjetpack
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.androidjetpack.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = Navigation.findNavController(view)
+
+        /*
+        navigation component
+         */
+        binding.tvPro.setOnClickListener {
+            navController.navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+
+}
