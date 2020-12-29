65558f0bbac3d11e7b9f024f2432ce086b138edf
Duong Minh Duc <ducdm.bhsoft@gmail.com>
2020-12-29 15:26:51 +0700

    init commit

--- app/src/main/java/com/example/androidjetpack/DoingWork.kt
+++ app/src/main/java/com/example/androidjetpack/DoingWork.kt
@@ -1,4 +1,39 @@
 package com.example.androidjetpack
 
-class DoingWork : Work {
+import android.content.Context
+import android.provider.Settings.Global.putString
+import android.util.Log.e
+import androidx.work.Data
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.androidjetpack.FirstFragment.Companion.DUC_NO_PRO
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DoingWork(
+    context: Context,
+    workerParams: WorkerParameters) : Worker(context, workerParams) {
+
+    companion object{
+        const val DUC_NO_PRO_2 = "duc_no_pro"
+    }
+
+
+    override fun doWork(): Result {
+        return try {
+            // get data
+            val count = inputData.getInt(DUC_NO_PRO,0)
+            e("abc", "ducNoPro $count")
+
+            // post data
+            val postData = "duc pro oo"
+            val outputData = Data.Builder()
+                    .putString(DUC_NO_PRO_2,postData)
+                    .build()
+
+            Result.success(outputData)
+        } catch (e: Exception) {
+            Result.failure()
+        }
+    }
 }
--- app/src/main/java/com/example/androidjetpack/FirstFragment.kt
+++ app/src/main/java/com/example/androidjetpack/FirstFragment.kt
@@ -1,22 +1,31 @@
 package com.example.androidjetpack
 
 import android.os.Bundle
+import android.util.Log.e
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.work.*
+import com.example.androidjetpack.DoingWork.Companion.DUC_NO_PRO_2
 import com.example.androidjetpack.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
 
+    companion object {
+        const val DUC_NO_PRO = "DucNoPro"
+    }
+
     private lateinit var binding: FragmentFirstBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false)
         return binding.root
     }
@@ -29,8 +38,48 @@ class FirstFragment : Fragment() {
         navigation component
          */
         binding.tvPro.setOnClickListener {
-            navController.navigate(R.id.action_firstFragment_to_secondFragment)
+            //  setOneTimeWorkRequest()
+            //  navController.navigate(R.id.action_firstFragment_to_secondFragment)
         }
+
+        /**
+        WorkManager : thực thi dưới background, bất đồng bộ, có thể lên lịch thực thi
+        OneTimeWorkRequest : thực thi 1 lần
+        Constraints : cài đặt thêm trong quá trình thực thi
+        Setting output and input data
+         */
+        setOneTimeWorkRequest()
+    }
+
+    private fun setOneTimeWorkRequest() {
+        val constraints = Constraints.Builder()
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build()
+
+        val data = Data.Builder()
+                .putInt(DUC_NO_PRO, 1)
+                .build()
+
+        val task = OneTimeWorkRequest.Builder(DoingWork::class.java)
+                .setConstraints(constraints)
+                .setInputData(data)
+                .build()
+
+        val workManager = WorkManager.getInstance(requireContext().applicationContext)
+        workManager.enqueue(task)
+        workManager.getWorkInfoByIdLiveData(task.id).observe(
+                viewLifecycleOwner, Observer { workInfo ->
+
+            if (workInfo != null && workInfo.state == WorkInfo.State.SUCCEEDED) {
+                binding.tvPro.text = workInfo.state.name
+            }
+
+            if (workInfo.state.isFinished) {
+                val dataDoingWork = workInfo.outputData.getString(DUC_NO_PRO_2)
+                e("af", dataDoingWork + "")
+            }
+        })
     }
 
 }
